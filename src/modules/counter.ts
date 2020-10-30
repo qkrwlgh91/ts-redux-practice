95cdcf83b630f38664ffcac483d4a5cc11941203
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-30 12:31:09 +0900

    added counter reducer in modules

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -33,3 +33,18 @@ const initialState: CounterState = {
     count: 0
 };
 
+// Reducer 작성
+function counter(state: CounterState = initialState, action: CounterAction) {
+    switch (action.type) {
+        case INCREASE:
+            return { count: state.count + 1 };
+        case DECREASE:
+            return { count: state.count - 1 };
+        case INCREASE_BY:
+            return { count: state.count + action.payload };
+        default:
+            return state;
+    }
+}
+
+export default counter;
