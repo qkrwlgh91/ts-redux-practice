4b71f2456416174dbcd6ac37afbe3886b3e5e6e6
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-31 20:09:45 +0900

    added custom hook for todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Todo } from '../modules/todos';
 import TodoItem from './TodoItem';
+import useTodos from '../hooks/useTodos';
 
 function TodoList() {
-    const todos: Todo[] = []; // TODO: 커스텀 Hook을 사용하여 조회
+    const todos: Todo[] = useTodos(); // TODO: 커스텀 Hook을 사용하여 조회
     
     if (todos.length === 0) return <p>등록된 항목이 없습니다.</p>;
     return (
