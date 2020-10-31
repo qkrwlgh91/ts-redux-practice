3f85ea77f83a85b2ddd09d352c82cfcf7c7b38b6
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-31 20:16:18 +0900

    added custom hook for todo insert function

--- src/components/TodoInsert.tsx
+++ src/components/TodoInsert.tsx
@@ -1,13 +1,17 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
+import useAddTodo from '../hooks/useAddTodo';
 
 function TodoInsert() {
     const[value, setValue] = useState('');
+    const addTodo = useAddTodo();
+
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     };
     const onSubmit = (e: FormEvent) => {
         e.preventDefault();
         // TODO: 커스텀 Hook을 이요한 새항목 등록
+        addTodo(value);
         setValue('');
     };
 
