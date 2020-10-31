2a839a2e8cf4b9bb8a16157e4114a561ca54c2a5
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-31 19:01:04 +0900

    added todo action type and initial state

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -9,6 +9,6 @@ const rootReducer = combineReducers({
 
 export default rootReducer;
 
-// reducer의 root를 생성할때 typescript없이 사용할 때와 거의 동일하지만 typescript를 사용할때는 RootState라는 타입을 만들어서 내보내주어야 하낟.
+// reducer의 root를 생성할때 typescript없이 사용할 때와 거의 동일하지만 typescript를 사용할때는 RootState라는 타입을 만들어서 내보내주어야 한다.
 // RootState타입은 컨테이너 컴포넌트에서 스토어에서 관리하고 있는 상태를 조회하기 위해서 useSelector를 사용할 때 필요하다.
 export type RootState = ReturnType<typeof rootReducer>;
