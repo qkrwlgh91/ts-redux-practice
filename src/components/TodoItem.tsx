37de4cf5c4ae3e3b67af3e8fb61c8684999fd45c
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-31 20:27:59 +0900

    added custom hook for delete and done function

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TodoItem.css';
 import { Todo } from '../modules/todos';
+import useTodoActions from '../hooks/useTodoActions';
 
 type TodoItemProps = {
     todo: Todo;
@@ -8,10 +9,12 @@ type TodoItemProps = {
 
 function TodoItem({ todo }: TodoItemProps) {
     // TODO: 커스텀 Hookfmf tkdydgotj onToggle / onRemove 구현
+    const { onToggle, onRemove } = useTodoActions(todo.id);
+
     return (
         <li className={`TodoItem ${todo.done ? 'done' : ''}`}>
-            <span className="text">{todo.text}</span>
-            <span className="remove">(X)</span>
+            <span className="text" onClick={onToggle}>{todo.text}</span>
+            <span className="remove" onClick={onRemove}>(X)</span>
         </li>
     );
 }
