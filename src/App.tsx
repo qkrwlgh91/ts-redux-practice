298b7bb6ce0bc4585f9a1c3cefbdbd26b12e1f44
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-01-18 16:37:57 +0900

    typescript react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect, Link} from 'react-router-dom';
 // import CounterContainer from './containers/CounterContainer';
 import Counter from './components/Counter';
 import TodoInset from './components/TodoInsert';
@@ -8,13 +9,16 @@ import GithubProfileLoader from './containers/GithubProfileLoader';
 function App() {
   return (
     //<CounterContainer />
-    <>
-      <Counter />
-      <TodoInset />
-      <TodoList />
-      <hr />
-      <GithubProfileLoader />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path="/counter" component={Counter} />
+        <Route exact={true} path="/todo-insert" component={TodoInset} />
+        <Route exact={true} path="/todo-list" component={TodoList} />
+        <Route exact={true} path="/git-info" component={GithubProfileLoader} />
+        {/* not fount */}
+        <Route component={() => <Redirect to="/" />} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
