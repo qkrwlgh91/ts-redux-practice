8401348ec308faa258d85c17229ae9a791573bbe
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-01-18 22:32:50 +0900

    horizontal prac...

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,8 +3,32 @@ import React from 'react';
 
 function Home() {
     return (
-        <>
-        </>
+        <div className="container">
+            <section>
+                <div className="content" data-0="transform:translateX(0%);" data-1000="transform:translateX(-400%);">
+                    <h2>Section One</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor, eum sit deserunt doloribus earum ex officiis id eius, laudantium nulla voluptas eaque fugit laborum placeat alias? Id, unde voluptatum!</p>
+                    </div>
+            </section>
+            <section>
+                <div className="content">
+                    <h2>Section Two</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor, eum sit deserunt doloribus earum ex officiis id eius, laudantium nulla voluptas eaque fugit laborum placeat alias? Id, unde voluptatum!</p>
+                    </div>
+            </section>
+            <section>
+                <div className="content">
+                    <h2>Section Three</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor, eum sit deserunt doloribus earum ex officiis id eius, laudantium nulla voluptas eaque fugit laborum placeat alias? Id, unde voluptatum!</p>
+                    </div>
+            </section>
+            <section>
+                <div className="content">
+                    <h2>Section Four</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolor, eum sit deserunt doloribus earum ex officiis id eius, laudantium nulla voluptas eaque fugit laborum placeat alias? Id, unde voluptatum!</p>
+                    </div>
+            </section>
+        </div>
     )
 }
 
